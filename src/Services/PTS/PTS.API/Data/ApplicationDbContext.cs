f649fec8a6106b6a7c4f85077b86c49bd6ad3988
Debayan Samaddar <debumist@gmail.com>
2025-04-06 14:59:26 +0600

    .

---
+++ src/Services/PTS/PTS.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PTS.API.Models.Domain;
+
+namespace PTS.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Project> Projects { get; set; }
+
+        public DbSet<Images> Images { get; set; }
+        public DbSet<Attachment> Attachments { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+
+
+    }
+}
