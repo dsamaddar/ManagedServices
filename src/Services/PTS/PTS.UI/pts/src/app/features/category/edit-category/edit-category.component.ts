a4f7187e7cdcf8e7934b85ac787f3e4bedeca3e9
Debayan Samaddar <debumist@gmail.com>
2025-04-09 10:26:55 +0600

    ui.edit.category

--- src/Services/PTS/PTS.UI/pts/src/app/features/category/edit-category/edit-category.component.ts
+++ src/Services/PTS/PTS.UI/pts/src/app/features/category/edit-category/edit-category.component.ts
@@ -1,11 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
 
 @Component({
   selector: 'app-edit-category',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './edit-category.component.html',
   styleUrl: './edit-category.component.css'
 })
-export class EditCategoryComponent {
+export class EditCategoryComponent implements OnInit, OnDestroy {
+
+  id?: number = 0;
+  paramsSubscription?: Subscription;
+  editCategorySubscription?: Subscription;
+  category?: Category;
+
+  constructor(private route: ActivatedRoute,
+    private categoryService: CategoryService,
+    private router: Router
+  ){
+
+  }
+
+  ngOnInit(): void {
+    this.paramsSubscription = this.route.paramMap.subscribe({
+      next: (params) => {
+        this.id = Number(params.get('id'));
+
+        if(this.id){
+          // get the data from the api for this category id
+          this.categoryService.getCategoryById(this.id)
+          .subscribe({
+            next: (response) => {
+              this.category = response;
+            }
+          });
+        }
+      }
+    });
+  }
+
+  
+
+  onFormSubmit():void{
+    const updateCategoryRequest: UpdateCategoryRequest = {
+      name: this.category?.name ?? '',
+      description: this.category?.description ?? ''
+    }
+
+    // pass this object to service
+    if(this.id){
+      this.editCategorySubscription = this.categoryService.updateCategory(this.id, updateCategoryRequest)
+      .subscribe({
+        next: (response) =>{
+          this.router.navigateByUrl('/admin/categories')
+        }
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
+    this.editCategorySubscription?.unsubscribe();
+  }
 
 }
--- src/Services/PTS/PTS.UI/pts/src/app/features/category/services/category.service.ts
+++ src/Services/PTS/PTS.UI/pts/src/app/features/category/services/category.service.ts
@@ -4,20 +4,40 @@ import { HttpClient } from '@angular/common/http';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { Category } from '../models/category.model';
 import { environment } from '../../../../environments/environment';
+import { UpdateCategoryRequest } from '../models/update-category-request.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) {  }
-
-  AddCategory(model: AddCategoryRequest): Observable<void>{
-    return this.http.post<void>(`${environment.apiBaseUrl}/api/Categories`, model);
+  AddCategory(model: AddCategoryRequest): Observable<void> {
+    return this.http.post<void>(
+      `${environment.apiBaseUrl}/api/Categories`,
+      model
+    );
   }
 
-  getAllCategories(): Observable<Category[]>{
-    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories`);
+  getAllCategories(): Observable<Category[]> {
+    return this.http.get<Category[]>(
+      `${environment.apiBaseUrl}/api/Categories`
+    );
   }
 
+  getCategoryById(id: number): Observable<Category> {
+    return this.http.get<Category>(
+      `${environment.apiBaseUrl}/api/Categories/${id}`
+    );
+  }
+
+  updateCategory(
+    id: number,
+    updateCategoryRequest: UpdateCategoryRequest
+  ): Observable<Category> {
+    return this.http.put<Category>(
+      `${environment.apiBaseUrl}/api/Categories/${id}`,
+      updateCategoryRequest
+    );
+  }
 }
