8637490045f55f102fd65efe5276ece0b4d01205
Debayan Samaddar <debumist@gmail.com>
2025-04-09 09:07:35 +0600

    ui.bootstrap.icons.installation.configuration

--- src/Services/PTS/PTS.UI/pts/src/app/features/category/add-category/add-category.component.ts
+++ src/Services/PTS/PTS.UI/pts/src/app/features/category/add-category/add-category.component.ts
@@ -3,6 +3,7 @@ import { FormsModule } from '@angular/forms';
 import { AddCategoryRequest } from '../models/add-category-request.model';
 import { CategoryService } from '../services/category.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -15,7 +16,9 @@ export class AddCategoryComponent implements OnDestroy {
   model: AddCategoryRequest;
   private addCategorySubscription?: Subscription;
 
-  constructor(private categoryService: CategoryService){
+  constructor(private categoryService: CategoryService,
+    private router: Router
+  ){
     this.model = {
       name: '',
       description: ''
@@ -27,7 +30,9 @@ export class AddCategoryComponent implements OnDestroy {
     this.addCategorySubscription = this.categoryService.AddCategory(this.model)
     .subscribe({
       next: (response) => {
+
         console.log('this was successful');
+        this.router.navigateByUrl('/admin/categories');
       },
       error: (error) => {
 
