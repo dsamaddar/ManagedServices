4471a20608e12d7a52831c76a41e3ba954927b9e
dsamaddar <debumist@gmail.com>
2025-04-10 20:24:55 +0600

    ui.add.product.fileuploader

--- src/Services/PTS/PTS.UI/pts/src/app/features/product/add-product/add-product.component.ts
+++ src/Services/PTS/PTS.UI/pts/src/app/features/product/add-product/add-product.component.ts
@@ -21,6 +21,8 @@ import { NumericLiteral } from 'typescript';
 import { Product } from '../models/product.model';
 import { AddProductRequest } from '../models/add-product.model';
 import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-add-product',
@@ -34,7 +36,6 @@ import { FormsModule } from '@angular/forms';
     MatDatepickerModule,
     NgSelectModule,
     FormsModule,
-    DatePipe
   ],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css',
@@ -42,19 +43,27 @@ import { FormsModule } from '@angular/forms';
 export class AddProductComponent implements OnInit, OnDestroy {
   
   product: AddProductRequest;
-  
+  selectedFiles: File[] = [];
 
   // observable array
   categories$?: Observable<Category[]>;
   cylinderCompanies$?: Observable<CylinderCompany[]>;
   printingCompanies$?: Observable<PrintingCompany[]>;
 
+  iconList = [ // array of icon class list based on type
+    { type: "docx", icon: "fas fa-file-word" },
+    { type: "xlsx", icon: "fas fa-file-excel" },
+    { type: "pdf", icon: "fas fa-file-pdf" },
+    { type: "jpg", icon: "fas fa-file-image" }
+  ];
+
   constructor(
     private categoryService: CategoryService,
     private cylinderCompanyService: CylindercompanyService,
     private printingCompanyService: PrintingcompanyService,
     private router: Router,
-    private datepipe: DatePipe
+    private datepipe: DatePipe,
+    private http: HttpClient
   ) {
     const myDate = new Date();
     const formatted = this.datepipe.transform(myDate, 'yyyy-MM-dd');
@@ -89,11 +98,55 @@ export class AddProductComponent implements OnInit, OnDestroy {
       this.printingCompanyService.getAllPrintingCompanies();
   }
 
+  onFilesSelected(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
+    if (fileInput.files) {
+      this.selectedFiles = Array.from(fileInput.files);
+    }
+  }
+
+  getFileExtension(filename : string) : string { 
+    let ext = filename.split(".").pop();
+    console.log(ext);
+    let obj = this.iconList.filter(row =>
+      {
+        if (row.type === ext) {
+          return true;
+        }
+
+        return "";
+      });
+    //console.log(obj);
+
+    if (obj.length > 0) {
+      let icon = obj[0].icon;
+      console.log(icon);
+      return icon;
+    } 
+    else {
+      console.log('not found');
+      return "";
+    }
+  }
+
   onFormSubmit() {
     this.product.categoryid = this.categoryid || 0;
     this.product.cylindercompanyid = this.cylindercompanyid || 0;
     this.product.printingcompanyid = this.printingcompanyid || 0;
-    
+
     console.log(this.product);
+
+    if (this.selectedFiles.length === 0) return;
+
+    const formData = new FormData();
+    this.selectedFiles.forEach(file => {
+      formData.append('files', file); // Use 'files[]' if backend expects array
+    });
+
+    this.http.post('https://your-api.com/upload', formData).subscribe({
+      next: (res) => console.log('Files uploaded!', res),
+      error: (err) => console.error('Upload failed:', err),
+    });
+
   }
 }
