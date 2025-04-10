6630b87cf8063c5eecccdbd0987d551ec784e598
Debayan Samaddar <debumist@gmail.com>
2025-04-10 14:50:34 +0600

    ui.ngselect.add.product

--- src/Services/PTS/PTS.UI/pts/src/app/features/product/add-product/add-product.component.ts
+++ src/Services/PTS/PTS.UI/pts/src/app/features/product/add-product/add-product.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatDatepicker, MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-add-product',
-  imports: [],
+  standalone:true,
+  imports: [MatSelectModule, MatFormFieldModule, MatInputModule, ReactiveFormsModule,
+    CommonModule, FormsModule, RouterModule, MatDatepickerModule, NgSelectModule
+  ],
   templateUrl: './add-product.component.html',
   styleUrl: './add-product.component.css'
 })
-export class AddProductComponent {
+export class AddProductComponent implements OnInit {
+  
+  items = [
+    { id: 1, name: 'Apple' },
+    { id: 2, name: 'Banana' },
+    { id: 3, name: 'Orange' }
+  ];
+  
+  selectedItem?: number;
+
+  ngOnInit(): void {
+  }
+
 
 }
