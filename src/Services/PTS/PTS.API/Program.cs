812c7042f386f064dea4b8e98e755a6073314652
dsamaddar <debumist@gmail.com>
2025-04-15 18:27:17 +0600

    api.new.migrations

--- src/Services/PTS/PTS.API/Program.cs
+++ src/Services/PTS/PTS.API/Program.cs
@@ -20,14 +20,12 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PTSConnectionString_OFFICE"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PTSConnectionString_MOAR"));
 });
 
 builder.Services.AddDbContext<AuthDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PTSConnectionString_OFFICE"))
-    .EnableSensitiveDataLogging()
-    .LogTo(Console.WriteLine,LogLevel.Information);
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PTSConnectionString_MOAR"));
 });
 
 builder.Services.AddScoped<IAttachmentRepository, AttachmentRepository>();
