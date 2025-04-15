93099b00c49666e21afb14a4ddb0cf57db91801b
Debayan Samaddar <debumist@gmail.com>
2025-04-15 14:00:26 +0600

    api.identity.authentication

---
+++ src/Services/PTS/PTS.API/Data/AuthDbContext.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PTS.API.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var readerRoleId = "637255cb-f655-4537-956a-075130dd9ac9";
+            var managerRoleId = "15e6e97c-401d-4ba1-b124-4ffdcb577f6a";
+            var adminRoleId = "1b9f33f8-0ff3-4669-9f87-89a5743efedb";
+
+            // Create Reader, Manager, and Writer Role
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole()
+                {
+                    Id = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper(),
+                    ConcurrencyStamp = readerRoleId
+                },
+                new IdentityRole()
+                {
+                    Id = managerRoleId,
+                    Name = "Manager",
+                    NormalizedName = "Manager".ToUpper(),
+                    ConcurrencyStamp = managerRoleId
+                },
+                new IdentityRole()
+                {
+                    Id = adminRoleId,
+                    Name = "Admin",
+                    NormalizedName = "Admin".ToUpper(),
+                    ConcurrencyStamp = adminRoleId
+                }
+            };
+
+            // Seed the roles
+            builder.Entity<IdentityRole>().HasData(roles);
+
+            // create a default Admin User
+            var adminUserId = "e07b4029-5a27-491d-9fc5-7043e22ae5eb";
+            var admin = new IdentityUser()
+            {
+                Id = adminUserId,
+                UserName = "debayan.samaddar@neoscoder.com",
+                Email = "debayan.samaddar@neoscoder.com",
+                NormalizedEmail = "debayan.samaddar@neoscoder.com".ToUpper(),
+                NormalizedUserName = "dsamaddar".ToUpper()
+            };
+
+            admin.PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(admin, "Farc1lgh#");
+            builder.Entity<IdentityUser>().HasData(adminUserId);
+
+            // Give Roles to Admin
+            var adminRoles = new List<IdentityUserRole<string>>()
+            {
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = readerRoleId,
+                },
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = managerRoleId,
+                },
+                new()
+                {
+                    UserId = adminUserId,
+                    RoleId = adminRoleId,
+                }
+            };
+
+            builder.Entity<IdentityUserRole<string>>().HasData(adminRoles);
+        }
+
+    }
+}
--- src/Services/PTS/PTS.API/Program.cs
+++ src/Services/PTS/PTS.API/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PTS.API.Data;
 using PTS.API.Middleware;
 using PTS.API.Repositories.Implementation;
 using PTS.API.Repositories.Interface;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Data.SqlClient;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +19,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PTSConnectionString_OFFICE"));
+});
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("PTSConnectionString_OFFICE"));
 });
@@ -31,6 +40,38 @@ builder.Services.Configure<FormOptions>(options =>
     options.MultipartBodyLengthLimit = 1073741824; // 1 GB in bytes
 });
 
+// configuring identity core
+builder.Services.AddIdentityCore<IdentityUser>()
+    .AddRoles<IdentityRole>()
+    .AddTokenProvider<DataProtectorTokenProvider<IdentityUser>>("neoscoder")
+    .AddEntityFrameworkStores<AuthDbContext>()
+    .AddDefaultTokenProviders();
+
+// configuring identity options
+builder.Services.Configure<IdentityOptions>(options => {
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
+// token validation parameters
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            AuthenticationType = "Jwt",
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audiance"],
+            IssuerSigningKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        };
+    });
 
 var app = builder.Build();
 
@@ -53,6 +94,7 @@ app.UseCors(options =>
     options.AllowAnyMethod();
 });
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
