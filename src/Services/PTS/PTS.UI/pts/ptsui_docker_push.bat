bc106ad78949d93e10429d1a9b71b3f59259299c
Debayan Samaddar <debumist@gmail.com>
2025-04-27 08:47:24 +0600

    docker.bat.file.final

--- src/Services/PTS/PTS.UI/pts/ptsui_docker_push.bat
+++ src/Services/PTS/PTS.UI/pts/ptsui_docker_push.bat
@@ -1,8 +1,7 @@
-echo Starting docker push process for PTSUI
+@echo off
 
-ng build --configuration production
-docker build -t ptsui .
-docker run -d -p 5203:5203 -p 5204:5204 ptsui
+call ng build --configuration production
+call docker build -t ptsui .
+call docker run -d -p 5203:5203 -p 5204:5204 ptsui
 
-echo process complete.
 pause
