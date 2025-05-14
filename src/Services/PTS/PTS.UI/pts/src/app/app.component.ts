a943645a574518c64c452951d7240095630f239b
Debayan Samaddar <debumist@gmail.com>
2025-05-14 12:57:41 +0600

    .

--- src/Services/PTS/PTS.UI/pts/src/app/app.component.ts
+++ src/Services/PTS/PTS.UI/pts/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './core/components/navbar/navbar.component';
-import { FooterComponent } from "./core/components/footer/footer.component";
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule,RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [CommonModule,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
